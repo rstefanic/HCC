703109b99f2eff22ae9b2ec2790ed8cbd7219bc0
Robert Stefanic <rstefanic72@gmail.com>
2018-11-09 23:32:18 -0600

    Modified test suite to read .c files

--- tests/tests.hs
+++ tests/tests.hs
@@ -11,26 +11,23 @@ test = do
         it "True is True" $ do
             True `shouldBe` True
 
-basicMain :: String
-basicMain = "int main(){\n\treturn 5;\n}"
-
-basicParsing :: Spec
-basicParsing = do
+basicParsing :: String -> Spec
+basicParsing file = do
     describe "A basic main function should compile" $ do
-        it "The Lexer should properly tokenize the input" $ do
-            HCC.Lexer.lex basicMain `shouldBe` 
-                Right [ T.TokenData T.TInt 1 4
-                      , T.TokenData (T.TId "main") 1 9
-                      , T.TokenData T.TOpenParen 1 10
-                      , T.TokenData T.TCloseParen 1 11
-                      , T.TokenData T.TOpenBrace 1 12
-                      , T.TokenData T.TReturn 2 15
-                      , T.TokenData (T.TIntLiteral 5) 2 17
-                      , T.TokenData T.TSemicolon 2 18
-                      , T.TokenData T.TCloseBrace 3 2
+        it "The Lexer should be properly tokenize the input" $ do
+            HCC.Lexer.lex file `shouldBe` 
+                Right [ T.TokenData T.TInt 1
+                      , T.TokenData (T.TId "main") 1
+                      , T.TokenData T.TOpenParen 1
+                      , T.TokenData T.TCloseParen 1
+                      , T.TokenData T.TOpenBrace 1
+                      , T.TokenData T.TReturn 2
+                      , T.TokenData (T.TIntLiteral 5) 2
+                      , T.TokenData T.TSemicolon 2
+                      , T.TokenData T.TCloseBrace 3
                       ]
-
 
 main :: IO ()
 main = do
-    hspec $ test
+    basicMain <- readFile "./tests/basic_main.c"
+    hspec $ basicParsing basicMain
